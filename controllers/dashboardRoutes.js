67fd31e1004d2acc4900d3bd920edb938d442fae
CaptainFlopp3y <barrancofabian@gmail.com>
2023-07-30 14:53:56 -0600

    working here

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// dashboard route to get all of a user's posts
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'date',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'body', 'date', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['name']
+                }
+            },
+            {
+                model: User,
+                attributes: ['name']
+            }
+        ]
+    })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            console.log(posts)
+            res.render('dashboard', {
+                posts,
+                userName: req.session.name,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// dashboard route to edit post found by id
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'date',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'body', 'date', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['name']
+                }
+            },
+            {
+                model: User,
+                attributes: ['name']
+            }
+        ]
+    })
+        .then(postData => {
+            if (postData) {
+                const post = postData.get({ plain: true });
+
+                res.render('editPost', {
+                    post,
+                    userName: req.session.name,
+                    loggedIn: true
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,108 @@
+const router = require('express').Router();
+const { Comment, Post, User } = require('../models');
+
+// get all posts to be shown on homepage without needing to be logged in
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'date',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'body', 'date', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['name']
+                }
+            },
+            {
+                model: User,
+                attributes: ['name']
+            }
+        ]
+    })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts,
+                userName: req.session.name,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get single post by id
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'date',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'body', 'date', 'user_id', 'post_id'],
+                include: {
+                    model: User,
+                    attributes: ['name']
+                }
+            },
+            {
+                model: User,
+                attributes: ['name']
+            }
+        ]
+    })
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({ message: 'Post not found!' });
+                return;
+            }
+
+            const post = postData.get({ plain: true });
+
+            res.render('singlePost', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// register route, if logged in redirect to homepage/dashboard
+router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+// login page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
